7d468001fe1fce7c2e154c4b8769c0bd3822ec9d
Murad Khasmammadov <65508668+muradkhasmammadov@users.noreply.github.com>
2025-07-27 22:58:56 +0400

    Add project files.

---
+++ MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using XalqBankTestAppFileMonitoring.Models;
+using XalqBankTestAppFileMonitoring.Services;
+
+namespace XalqBankTestApp
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<FileData> FileData { get; } = new();
+        private readonly FileMonitorService monitor;
+
+        public MainViewModel()
+        {
+            monitor = new FileMonitorService();
+            monitor.OnDataLoaded += filedata =>
+            {
+                App.Current.Dispatcher.Invoke(() => FileData.Add(filedata));
+            };
+            monitor.Start();
+        }
+    }
+}
